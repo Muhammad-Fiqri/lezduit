19bfe327ed316e2f06a89f688f9f38e2620c5b69
Muhammad <you@example.com>
2023-04-28 22:50:59 +0700

    Task List Tab Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TopNav from "./Task Menu/TopNav";
 import YourTask from "./Task Menu/YourTask";
+import TaskList from "./Task Menu/TaskList";
 
 function App() {
   return(
     <>
       <TopNav/>
       <YourTask/>
+      <TaskList/>
     </>
   );
 }
---
+++ src/Task Menu/TaskList.jsx
@@ -0,0 +1,13 @@
+import './TaskList.scss';
+
+function TaskList() {
+    return(
+        <>
+            <div className="TaskListWrapper">
+                <div className="ToDo">To-Do 1</div>
+            </div>
+        </>
+    );
+}
+
+export default TaskList;
