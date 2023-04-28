27bf468f5c1dc84987e137f36f117b7bf06cef22
Muhammad <you@example.com>
2023-04-28 22:21:31 +0700

    Top Navigation Done

---
+++ src/TopNav.jsx
@@ -0,0 +1,15 @@
+import AddIcon from '@mui/icons-material/Add';
+import './TopNav.scss';
+
+function TopNav() {
+    return(
+      <>
+        <div className='TopNavWrapper'>
+            <h1>LEZDUIT!</h1>
+            <AddIcon/>
+        </div>
+      </>
+    );
+}
+  
+export default TopNav;
